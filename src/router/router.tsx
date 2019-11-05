e8772ac3d6db4b120291cd9467bba289794ce630
tianlman <tianlman@163.com>
2019-11-05 10:35:12 +0800

    配置less

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Loadable from 'react-loadable'
 import {Redirect} from "react-router-dom";
-import Layout from "../Layout/Layout";
 
 //标记： 在定义我们的路由对象，使用react-loadable 对路由组件进行懒加载，这是经常需要做的行为。
 // 详情请参考这一篇文章：https://blog.csdn.net/China_Guanq/article/details/82194928#loadable

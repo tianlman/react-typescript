0cd3996d38f4950014d949c849fa1af91ff34019
tianlman <tianlman@163.com>
2019-11-08 11:48:13 +0800

    redux

--- src/page/Ceshi/Ceshi.tsx
+++ src/page/Ceshi/Ceshi.tsx
@@ -1,30 +1,53 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import {StoreState} from "../../redux/type";
+import {decrement, increment} from "../../redux/store/actions";
 
-export interface Props {
-    name: string;
-    enthusiasmLevel?: number;
-    onIncrement?: () => void;
-    onDecrement?: () => void;
+// import { decrement, increment } from '../store/actions';
+// import { StoreState } from '../types';
+
+
+// 创建类型接口
+export interface IProps {
+    value: number,
+    onIncrement: () => void,
+    onDecrement: () => void
 }
 
-export default function Test({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
-    if (enthusiasmLevel <= 0) {
-        throw new Error('You could be a little more enthusiastic. :D');
+// 使用接口代替 PropTypes 进行类型校验
+class Ceshi extends React.PureComponent<IProps,any> {
+    public componentWillMount () {
+        console.log(this.props,'1111111') // 这里的prop是拿不到dispatch函数，因为组合高阶函数的时候做了处理，没有传入dispatch，只有{value: 0, onDecrement: ƒ, onIncrement: ƒ}
     }
 
-    return (
-        <div className="hello">
-            <div className="greeting">
-                Hello {name + getExclamationMarks(enthusiasmLevel)}
-            </div>
-            <div>
-                <button onClick={onDecrement}>-</button>
-                <button onClick={onIncrement}>+</button>
-            </div>
-        </div>
-    );
+    public render() {
+        console.log(this.props,'1111111')
+        const { value, onIncrement, onDecrement } = this.props;
+        return (
+            <p>
+                Clicked: { value } times
+                <br />
+                <br />
+                <button onClick={ onIncrement } style={{ marginRight: 20 }}> +  </button>
+                <button onClick={ onDecrement }> - </button>
+            </p>
+        )
+    }
 }
 
-function getExclamationMarks(numChars: number) {
-    return Array(numChars + 1).join('!');
-}
+// 将 reducer 中的状态插入到组件的 props 中
+// 下面是单个reducer的时候，多个的时候需要选传入哪个reducer
+// const { test, count } = state
+const mapStateToProps = (state: StoreState): { value: number } => ({
+    value: state
+})
+
+// 将 对应action 插入到组件的 props 中
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    onDecrement: () => dispatch(decrement()),
+    onIncrement: () => dispatch(increment())
+})
+
+// 使用 connect 高阶组件对 Counter 进行包裹
+export default connect(mapStateToProps, mapDispatchToProps)(Ceshi);
